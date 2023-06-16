948dce340f427c5a64d415a4b08344df80fd9077
admin <admin@YD02-17>
2023-06-16 17:17:42 +0900

    commons-fileupload json 라이브러리 추가

--- magicCode/src/main/java/com/withpuppy/magicCode/member/common/FileUpload.java
+++ magicCode/src/main/java/com/withpuppy/magicCode/member/common/FileUpload.java
@@ -8,6 +8,10 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 
 
 public class FileUpload {
